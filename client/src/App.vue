70940d046f67555793a5770715bfce960ffe8f3e
CallumKerrEdwards <github@callumkerredwards.co.uk>
2022-08-26 10:29:41 +0100

    Moving client to https://github.com/CallumKerrEdwards/library-client

--- client/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="p-14 ">
-        <router-link class="flex justify-center text-4xl text-gray-700" to="/">Library</router-link>
-        <router-view />
-    </div>
-</template>
-
-<style>
-</style>
